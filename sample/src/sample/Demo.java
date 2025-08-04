643792aec18ceb85289332f6c6e3c605a3300b11
koushika20 <koushikachintha@gmail.com>
2025-08-04 18:48:57 +0530

    success

---
+++ sample/src/sample/Demo.java
@@ -0,0 +1,21 @@
+
+package sample;
+class Example
+{
+	void print()
+	{
+		System.out.println("Welcome to TNS C2TC program");
+		}
+}
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Example ex=new Example();
+		ex.print();
+
+	}
+
+}
