ae4a759710be7bc501323645aad813767150c603
koushika20 <koushikachintha@gmail.com>
2025-08-05 13:39:26 +0530

    successful

---
+++ sample/src/sample/VariableDemo.java
@@ -0,0 +1,16 @@
+//types of variables in java
+package sample;
+
+public class VariableDemo {
+	
+	int a=20; //instance variable
+	String name="koushika";
+	static int age=20; //static variable
+	void display()
+	{
+		String college="KPRIT";//local variables
+		String branch="CSE";
+		System.out.println(college+" "+branch);
+		}
+
+}
---
+++ sample/src/sample/VariableDemoMain.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class VariableDemoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VariableDemo ob=new VariableDemo();
+		ob.display();  //calling method
+		System.out.println(ob.name);  //printing instance variable using object refer
+		System.out.println(VariableDemo.age);
+		
+		
+
+	}
+
+}
