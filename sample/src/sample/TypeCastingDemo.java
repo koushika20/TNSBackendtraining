1484e5cfa6bd453e72e54bb83bbda5a4f35b2a97
koushika20 <koushikachintha@gmail.com>
2025-08-05 13:48:56 +0530

    casting datatypes

---
+++ sample/src/sample/TypeCastingDemo.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args) {
+		//implicit type casting
+		short a=10;
+		int b=a;
+		float c=2;
+		double d=c;
+		System.out.println(d);
+		System.out.println(b);
+		
+		int f=20;
+		short e=(short)f;
+		System.out.println(e);
+		
+		double g=2.00000;
+		float h=(float)g;
+		System.out.print(h);
+		
+		
+		
+
+	}
+
+}
